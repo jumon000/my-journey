f316110d72927fe799b171e443538c154358ee35
Juman <150572273+jumon000@users.noreply.github.com>
2026-07-18 14:05:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (48.72%) - LeetSync

---
+++ Leetcode/3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        
+        int oddsum=n*(2*1+(n-1)*2)/2;
+        int evensum=n*(2*2+(n-1)*2)/2;
+
+        return std::gcd(oddsum,evensum);
+    }
+};
