37d6c1d9da9ed3453762086ae409c4d1a704b3f0
Juman <150572273+jumon000@users.noreply.github.com>
2026-07-27 14:51:10 +0530

    Time: 885 ms (9.52%) | Memory: 0B (100.00%) - LeetSync

---
+++ Leetcode/1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -0,0 +1,11 @@
+SELECT
+    m.employee_id,
+    m.name,
+    COUNT(e.employee_id) AS reports_count,
+    ROUND(AVG(e.age)) AS average_age
+FROM Employees e
+RIGHT JOIN Employees m
+ON e.reports_to = m.employee_id
+GROUP BY m.employee_id, m.name
+HAVING COUNT(e.employee_id) > 0
+ORDER BY m.employee_id;
