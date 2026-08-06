57e861f77ba28ca4e27646ffe681b8c56e79d325
Juman <150572273+jumon000@users.noreply.github.com>
2026-08-06 18:46:34 +0530

    Time: 252 ms (52.38%) | Memory: 321.5 MB (50.68%) - LeetSync

---
+++ Leetcode/3561-remove-methods-from-project/remove-methods-from-project.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+
+        queue<int> q;
+        vector<int> indegree(n, 0);
+        vector<vector<int>> adj(n);
+
+        for (auto &it : invocations) {
+            adj[it[0]].push_back(it[1]);
+            indegree[it[1]]++;
+        }
+
+        unordered_set<int> st;
+
+        q.push(k);
+        st.insert(k);
+
+        while (!q.empty()) {
+
+            int node = q.front();
+            q.pop();
+
+            for (auto it : adj[node]) {
+                indegree[it]--;
+
+                if (!st.count(it)) {
+                    q.push(it);
+                    st.insert(it);
+                }
+            }
+        }
+
+        bool flag = false;
+
+        for (auto &it : invocations) {
+            int u = it[0];
+            int v = it[1];
+
+            if (!st.count(u) && st.count(v)) {
+                flag = true;
+                break;
+            }
+        }
+
+        vector<int> ans;
+
+        if (flag) {
+            for (int i = 0; i < n; i++)
+                ans.push_back(i);
+            return ans;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!st.count(i))
+                ans.push_back(i);
+        }
+
+        return ans;
+    }
+};
